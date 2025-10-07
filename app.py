e28849f1945390f74879d9953329b584c93072a7
calderaevelyn016 <23308060610294@cetis61.edu.mx>
2025-10-07 11:44:55 -0500

    gg

--- app.py
+++ app.py
@@ -9,22 +9,22 @@ def index():
     return render_template("index.html", nombre = autor, amigos = arr)
     
 @app.route("/1")
-def p1():
+def pp1():
     arr =["Daniela", "Leonardo", "Loretto","Luis"]
     autor = "Evelyn Fernanda Caldera Gallegos"
-    return render_template("p1.html", nombre = autor, amigos = arr)
+    return render_template("pp1.html", nombre = autor, amigos = arr)
 
 @app.route("/2")
-def p2():
+def pp2():
     arr =["1", "2", "3","4"]
     autor = "Evelyn Fernanda Caldera Gallegos"
-    return render_template("p2.html", nombre = autor, amigos = arr)
+    return render_template("pp2.html", nombre = autor, amigos = arr)
 
 @app.route("/3")
-def p3():
+def pp3():
     arr =["azul", "rojo", "verde","morado"]
     autor = "Evelyn Fernanda Caldera Gallegos"
-    return render_template("p3.html", nombre = autor, amigos = arr)
+    return render_template("pp3.html", nombre = autor, amigos = arr)
 
 if __name__ == "__main__":
     app.run(debug=True)
