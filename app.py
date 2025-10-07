6cb7a2166101d4c28f2111ae1a5923dbe42069dd
calderaevelyn016 <23308060610294@cetis61.edu.mx>
2025-10-07 11:36:17 -0500

    gg

--- app.py
+++ app.py
@@ -14,11 +14,17 @@ def p1():
     autor = "Evelyn Fernanda Caldera Gallegos"
     return render_template("p1.html", nombre = autor, amigos = arr)
 
-@app.route("/1")
+@app.route("/2")
 def p2():
     arr =["1", "2", "3","4"]
     autor = "Evelyn Fernanda Caldera Gallegos"
-    return render_template("p2.html", nombre = autor, num = arr)
+    return render_template("p2.html", nombre = autor, amigos = arr)
+
+@app.route("/3")
+def p3():
+    arr =["azul", "rojo", "verde","morado"]
+    autor = "Evelyn Fernanda Caldera Gallegos"
+    return render_template("p3.html", nombre = autor, amigos = arr)
 
 if __name__ == "__main__":
     app.run(debug=True)
